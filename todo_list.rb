325ce6c9df8cd1aa42a6a804a2557d61f390493a
Faisal Al-Tameemi <faisal.al.tameemi@gmail.com>
2016-07-04 20:09:56 -0400

    added some solutions

--- todo_list.rb
+++ todo_list.rb
@@ -1,16 +1,21 @@
 require 'active_record'
+require 'pry'
 
 class TodoList < ActiveRecord::Base
+  # relationships
   has_many :todos, dependent: :destroy
+
+  # validations
+  validates :title, presence: true
 
   # @description: adds a new todo item to the DB under this list
   def create_list_item(text)
-    # TODO: implement
+    todos.create(text: text, status: false)
   end
 
   # @description: returns a percentage completion based on all tasks within this list
   def completion_percentage
-  	# TODO: implement
+  	(todos.where(status: true).length / todos.all.length.to_f)*100
   end
 
   # CLASS METHODS
@@ -18,14 +23,16 @@ class TodoList < ActiveRecord::Base
 
     # @description: creates a new instance of TodoList in the DB and returns it
     # @hint: can we use what AR provides out of the box for this?
+    #        what value does doing it this way provide? can this value be obtained in another way?
     def new_list(title)
       # TODO: implement
     end
 
     # @description: finds a todo list from the DB by its title
     # @hint: can we use what AR provides out of the box for this?
+    #        what value does doing it this way provide?
     def find_list_by_title(query)
-      # TODO: implement
+      TodoList.where("title LIKE '%#{query}%'")
     end
 
   end
